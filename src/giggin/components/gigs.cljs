c130ed09dc679f6ed696427c8c4ce977a9795ddf
Christiaan Janssen <chistiaan.janssen@divider.net>
2018-11-20 06:11:59 +0100

    Video 06 - List Comprehension

--- src/giggin/components/gigs.cljs
+++ src/giggin/components/gigs.cljs
@@ -4,12 +4,14 @@
 (defn gigs
   []
   [:main
-   [:div.gigs (map (fn []
-                     [:div.gigs
-                      [:img.gig__artwork {:src "src" :alt "alt"}]
-                      [:div.gig__body
-                       [:div.gig__title
-                        [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
-                         [:i.icon.icon--plus]] "title"]
-                       [:p.gig__price "price"]4
-                       [:p.gig__desc "desc"]]])@state/gigs)]])
+   [:div.gigs
+    (for [{:keys [id img title price desc]} (vals @state/gigs)]
+      [:div.gig {:key id}
+       [:img.gig__artwork {:src img :alt title}
+        [:div.gig__body
+         [:div.gig__title
+          [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
+           [:i.icon.icon--plus]] title]
+         [:p.gig__price price]
+         [:p.gig__desc desc]]]])]])
+
