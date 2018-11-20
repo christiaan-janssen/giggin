85edf3f34eb10380ec3b5ca07231f37a3b90438e
Christiaan Janssen <chistiaan.janssen@divider.net>
2018-11-20 06:28:01 +0100

    Video 07 - Add to order

--- src/giggin/components/gigs.cljs
+++ src/giggin/components/gigs.cljs
@@ -7,11 +7,13 @@
    [:div.gigs
     (for [{:keys [id img title price desc]} (vals @state/gigs)]
       [:div.gig {:key id}
-       [:img.gig__artwork {:src img :alt title}
-        [:div.gig__body
-         [:div.gig__title
-          [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
-           [:i.icon.icon--plus]] title]
-         [:p.gig__price price]
-         [:p.gig__desc desc]]]])]])
+       [:img.gig__artwork {:src img :alt title}]
+       [:div.gig__body
+        [:div.gig__title
+         [:div.btn.btn--primary.float--right.tooltip
+          {:data-tooltip "Add to order"
+           :on-click (fn [] (swap! state/orders update id inc))}
+          [:i.icon.icon--plus]] title]
+        [:p.gig__price price]
+        [:p.gig__desc desc]]])]])
 
